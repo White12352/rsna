cb337482a1caceb079eeb4868de4f4680517dfd1
xchacha20-poly1305 <139959885+xchacha20-poly1305@users.noreply.github.com>
2023-09-16 09:01:02 +0800

    fix: run TUN in AppImage

--- go/cmd/updater/launcher_linux.go
+++ go/cmd/updater/launcher_linux.go
@@ -13,20 +13,32 @@ var local_qt_theme bool
 func Launcher() {
 	log.Println("Running as launcher")
 	wd, _ := filepath.Abs(".")
+	nd := filepath.Join(wd, "./nekoray") // nekoray's dictory, not your:)
 
 	_debug := flag.Bool("debug", false, "Debug mode")
 	flag.Parse()
 
-	cmd := exec.Command("./nekoray", flag.Args()...)
+	cmd := exec.Command(nd, flag.Args()...)
 
 	ld_env := "LD_LIBRARY_PATH=" + filepath.Join(wd, "./usr/lib")
 	qt_plugin_env := "QT_PLUGIN_PATH=" + filepath.Join(wd, "./usr/plugins")
+
+	// Check AppImage
+	// _, ok := os.LookupEnv("APPIMAGE")
+	// if ok {
+	// 	enabledTUN := os.Getenv("NKR_APPIMAGE_TUN")
+	// 	if enabledTUN == "1" || enabledTUN == "true" {
+	// 		// cmd.Args = append(cmd.Args, "-flag_restart_tun_on")
+	// 		cmd = exec.Command("sudo", append(append([]string{nd}, flag.Args()...), "-flag_restart_tun_on")...)
+	// 	}
+	// }
 
 	// Qt 5.12 abi is usually compatible with system Qt 5.15
 	// But use package Qt 5.12 by default.
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "NKR_FROM_LAUNCHER=1")
 	cmd.Env = append(cmd.Env, ld_env, qt_plugin_env)
+
 	log.Println(ld_env, qt_plugin_env, cmd)
 
 	if *_debug {
--- libs/package_appimage.sh
+++ libs/package_appimage.sh
@@ -21,7 +21,13 @@ cat >nekoray.AppDir/AppRun <<-EOF
 #!/bin/bash
 echo "PATH: \${PATH}"
 echo "NekoXray runing on: \$APPDIR"
-LD_LIBRARY_PATH=\${APPDIR}/usr/lib QT_PLUGIN_PATH=\${APPDIR}/usr/plugins \${APPDIR}/nekoray -appdata "\$@"
+if [[ \${NKR_APPIMAGE_TUN} == "1" ]] || [[ \${NKR_APPIMAGE_TUN} == "true" ]]; then
+    permissionCmd=\$(type -p pkexec || type -p gksudo || type -p sudo)
+fi
+
+export LD_LIBRARY_PATH=\${APPDIR}/usr/lib
+export QT_PLUGIN_PATH=\${APPDIR}/usr/plugins
+\${permissionCmd} \${APPDIR}/nekoray -appdata \$@
 EOF
 
 chmod +x nekoray.AppDir/AppRun
